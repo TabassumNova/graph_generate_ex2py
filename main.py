c19d4e4211d83a878a6d1f540771cff100a2835c
Tabassum Nova <tabassumnova1@gmail.com>
2024-01-23 10:41:48 +0100

    updated

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from demo3 import *
 
 if __name__ == "__main__":
     layout = plot_scenewise(plotly_viz=True)
-    layout.write_html('demo3.html')
+    layout.write_html('demo3.html', include_plotlyjs='cdn')
